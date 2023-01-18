169a97ab3637c7c452e3971dcf0154bf4da0ed0e
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-01-18 21:32:01 +1100

    3rd attempt to fix image error

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,16 +5,16 @@ export default function Footer() {
 		<footer className="footer">
 		<div className="socialMedia">
 			<a href="https://twitter.com/jinxlearns2code" alt="twitter-profile" target="_blank">
-				<img src="/twitter-icon.png" alt="twitter-icon" className="socialMedia-icon" />
+				<img src="twitter-icon.png" alt="twitter-icon" className="socialMedia-icon" />
 			</a>
 			<a href="https://www.facebook.com/kayle6694" alt="facebook-profile" target="_blank">
-				<img src="/facebook-icon.png" alt="facebook-icon" className="socialMedia-icon" />
+				<img src="facebook-icon.png" alt="facebook-icon" className="socialMedia-icon" />
 			</a>
 			<a href="https://www.instagram.com/abesamiskayle/" alt="instagram-profile" target="_blank">
-				<img src="/instagram-icon.png" alt="instagram-icon" className="socialMedia-icon" />
+				<img src="instagram-icon.png" alt="instagram-icon" className="socialMedia-icon" />
 			</a>
 			<a href="https://github.com/jinxlearns2code" alt="github-profile" target="_blank">
-				<img src="/github-icon.png" alt="github-icon" className="socialMedia-icon" />
+				<img src="github-icon.png" alt="github-icon" className="socialMedia-icon" />
 			</a>
 		</div>
 		</footer>
--- components/Image.jsx
+++ components/Image.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
 export default function Header() {
-	return <img src="/photo.jpg" alt="profile-photo" className="profile-photo" />
+	return <img src="photo.jpg" alt="profile-photo" className="profile-photo" />
 }
 
