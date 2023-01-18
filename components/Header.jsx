44c96b33dc8dde9f5912cf180f4d6ac4e7f87b16
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-01-18 21:20:48 +1100

    2nd attempt to fix image error

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
 				<a href="https://jinxlearns2code.github.io/" alt="github-link" target="_blank" className="website">https://jinxlearns2code.github.io/</a>
 			</header>
 			<div className="buttons">
-				<a href="mailto:jinxlearns2code@gmail.com" alt="email-address" className="email-address" target="_blank"><img src="../dist/images/mail-icon.png" className="mail-icon" />Email</a>
-				<a href="https://au.linkedin.com/in/kayle-abesamis-96b95991" alt="linkedin-profile" className="linkedin-profile" target="_blank"><img src="../dist/images/linkedin-icon.png" className="linkedin-icon" />LinkedIn</a>
+				<a href="mailto:jinxlearns2code@gmail.com" alt="email-address" className="email-address" target="_blank"><img src="/mail-icon.png" className="mail-icon" />Email</a>
+				<a href="https://au.linkedin.com/in/kayle-abesamis-96b95991" alt="linkedin-profile" className="linkedin-profile" target="_blank"><img src="/linkedin-icon.png" className="linkedin-icon" />LinkedIn</a>
 			</div>
 		</div>
 	)
