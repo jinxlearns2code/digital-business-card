9c4aed143d683daee396898a2e2e979c95e2507f
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-01-19 00:15:11 +1100

    deploy test

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,16 +5,16 @@ export default function Footer() {
 		<footer className="footer">
 		<div className="socialMedia">
 			<a href="https://twitter.com/jinxlearns2code" alt="twitter-profile" target="_blank">
-				<img src="public/images/twitter-icon.png" alt="twitter-icon" className="socialMedia-icon" />
+				<img src="images/twitter-icon.png" alt="twitter-icon" className="socialMedia-icon" />
 			</a>
 			<a href="https://www.facebook.com/kayle6694" alt="facebook-profile" target="_blank">
-				<img src="public/images/facebook-icon.png" alt="facebook-icon" className="socialMedia-icon" />
+				<img src="images/facebook-icon.png" alt="facebook-icon" className="socialMedia-icon" />
 			</a>
 			<a href="https://www.instagram.com/abesamiskayle/" alt="instagram-profile" target="_blank">
-				<img src="public/images/instagram-icon.png" alt="instagram-icon" className="socialMedia-icon" />
+				<img src="images/instagram-icon.png" alt="instagram-icon" className="socialMedia-icon" />
 			</a>
 			<a href="https://github.com/jinxlearns2code" alt="github-profile" target="_blank">
-				<img src="public/images/github-icon.png" alt="github-icon" className="socialMedia-icon" />
+				<img src="images/github-icon.png" alt="github-icon" className="socialMedia-icon" />
 			</a>
 		</div>
 		</footer>
