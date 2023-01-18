90bf077fc925483ad76627bd176933311706dc33
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-01-18 22:38:22 +1100

    testing

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
 				<a href="https://jinxlearns2code.github.io/" alt="github-link" target="_blank" className="website">https://jinxlearns2code.github.io/</a>
 			</header>
 			<div className="buttons">
-				<a href="mailto:jinxlearns2code@gmail.com" alt="email-address" className="email-address" target="_blank"><img src="/images/mail-icon.png" className="mail-icon" />Email</a>
+				<a href="mailto:jinxlearns2code@gmail.com" alt="email-address" className="email-address" target="_blank"><img src="public/images/mail-icon.png" className="mail-icon" />Email</a>
 				<a href="https://au.linkedin.com/in/kayle-abesamis-96b95991" alt="linkedin-profile" className="linkedin-profile" target="_blank"><img src="/images/linkedin-icon.png" className="linkedin-icon" />LinkedIn</a>
 			</div>
 		</div>
